9ac3cf47f987c539382311fde73c5f36d7d9af17
Şule İlkbahar <suleilkbahar999@gmail.com>
2024-09-07 12:01:38 +0300

    second commit for list

--- hotwarts.py
+++ hotwarts.py
@@ -1,4 +1,7 @@
 students =["hermonie","Harry","Ron"]
-for student in students:
-    print(student)
+# for student in students:
+#     print(student)
+for i in range(len(students)):
+    print(students[i])
+    
     
