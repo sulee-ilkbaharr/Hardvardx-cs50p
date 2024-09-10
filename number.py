dd5a84eef33cd772eac37974cb4ec27be4751828
Şule İlkbahar <suleilkbahar999@gmail.com>
2024-09-10 19:52:02 +0300

    exceptions updated

--- number.py
+++ number.py
@@ -6,3 +6,39 @@ except ValueError:
 else:
     #yukarıdaki kod başarılı olursa çalışır.(try).Eğer else ifaddesi olmazsa hata olmuş olsa bile bu kod çalışır ama biz else ile bunu önlüyoruz!
     print(f"x is {number}")
+    
+    
+    
+print(f"""
+      ************************************************************************
+      """)
+ 
+ 
+
+def main2():
+    x= get_int()
+    print(f"x is {x}")
+    
+
+def get_int():
+    while True:
+        try:
+            return int(input("What is x?"))
+        except ValueError:
+            pass
+
+
+
+def main3():
+    x= get_int("What is x?")
+    print(f"x is {x}")
+    
+
+def get_int(prompt):
+    while True:
+        try:
+            return int(input(prompt))
+        except ValueError:
+            pass
+
+main3()
