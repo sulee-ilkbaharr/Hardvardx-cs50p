a5c6ac3e05d165a429b68659777361ed9957ba1d
Şule İlkbahar <suleilkbahar999@gmail.com>
2024-09-13 23:27:12 +0300

    pytest added

--- test_calculator.py
+++ test_calculator.py
@@ -1,12 +1,28 @@
 from calculator import square
 def main():
-    test_square()
+     test_negatif
+     test_positif
+     test_zero
     
-def test_square():
-    if square(2)!=4:
-        print("2 square was not 4!")
-    if square(3)!=9:
-        print("2 square was not 9!")
+# def test_square():
+#     try:
+#         assert square(2)==4
+#     except AssertionError:
+#         print("2 square was not 4!")
+#     try:
+#         assert square(3)==9
+#     except AssertionError:
+#         print("3 square was not 9")
+
+def test_positif():
+    assert square(2)==4
+    assert square(3)==9
+def test_negatif():
+    assert square(-2)==4
+    assert square(-3)==9
+def test_zero():
+    assert square(0)==0
+    
         
         
 if __name__ == "__main__":
