c863445be1ee1ed37b0711ddfc120ec3f62a1a14
Şule İlkbahar <suleilkbahar999@gmail.com>
2024-09-24 23:37:51 +0300

    first commit for twitter.py

---
+++ format.py
@@ -0,0 +1,8 @@
+import re
+name = input("What is your name?").strip()
+
+if matches:=re.search(r"^(.+),(.+)$" ,name):
+    name=matches.group(2) + " " + matches.group(1)
+    
+    
+print(f"hello,{name}")
