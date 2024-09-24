f980a2880ae4a30c5e6dc6061a821892b84466ef
Şule İlkbahar <suleilkbahar999@gmail.com>
2024-09-25 00:16:50 +0300

    updates twitter.py

--- twitter.py
+++ twitter.py
@@ -1,4 +1,10 @@
+import re
 url = input("URL: ").strip()
 
-username = url.replace("https://twitter.com/" ,"")
-print(f"username: ",username)
+# username = re.sub("https://twitter.com/", "", url)
+# print(f"username: ",username)
+if mathes := re.search(r"^https?://(?:www\.)?twitter\.com/(.+)$", url , re.IGNORECASE):
+    print(f"username:", mathes.group(1))
+    
+    
+# ?:  means dont caches this for to groping
