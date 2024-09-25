2bd40a6293d773585f753eaa26eecea1afbec3d6
Şule İlkbahar <suleilkbahar999@gmail.com>
2024-09-26 00:27:46 +0300

    groups.py added for re

--- groups.py
+++ groups.py
@@ -2,7 +2,7 @@ import re
 location={"+1":"United Sattes and Canada","+62":"Indonesia","+505":"Nicaragua"}
 
 def main():
-    pattern= r"(\+\d{1,3}) \d{3}-\d{3}-\d{4}"
+    pattern= r"(?P<country_group>\+\d{1,3}) \d{3}-\d{3}-\d{4}"
     number = input("Number:")
     
     match = re.search(pattern,number)
