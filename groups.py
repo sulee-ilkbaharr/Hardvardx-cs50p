58299336c019e48b5c5dcb7e2f6c53ff02262717
Şule İlkbahar <suleilkbahar999@gmail.com>
2024-09-26 00:25:59 +0300

    groups.py added for re

---
+++ groups.py
@@ -0,0 +1,15 @@
+import re
+location={"+1":"United Sattes and Canada","+62":"Indonesia","+505":"Nicaragua"}
+
+def main():
+    pattern= r"(\+\d{1,3}) \d{3}-\d{3}-\d{4}"
+    number = input("Number:")
+    
+    match = re.search(pattern,number)
+    if match:
+        country_code = match.group(1)
+        print(location[country_code])
+    else:
+        print("Invalid")
+        
+main()
