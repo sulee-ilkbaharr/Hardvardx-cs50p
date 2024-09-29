0c00ff50b60e631b29a2619c7316db345da03a8d
Şule İlkbahar <suleilkbahar999@gmail.com>
2024-09-29 11:35:39 +0300

    unpack adde

---
+++ unpack.py
@@ -0,0 +1,8 @@
+def total(galleons, sickles, knuts):
+    return (galleons*17+sickles)*29 +knuts
+
+# coins=[100,50,25]
+# print(total(*coins), "knuts")
+
+coins={"galleons":100, "sickles":50,"knuts":25}
+print(total(**coins), "knuts")
