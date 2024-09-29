764e5a9348121091871c141535549c8ad40618a0
Şule İlkbahar <suleilkbahar999@gmail.com>
2024-09-29 16:36:49 +0300

    updated

--- gryffindor.py
+++ gryffindor.py
@@ -4,9 +4,16 @@ students=[
     {"name": "Ron", "house":"Gryffindor"},
     {"name": "Dracoy", "house":"Sltyherin"}
 ]
-gryffindor = [
-    student["name"] for student in students if student["house"]=="Gryffindor"
-]
+# gryffindor = [
+#     student["name"] for student in students if student["house"]=="Gryffindor"
+# ]
 
-for gryffindor in sorted(gryffindor):
-    print(gryffindor)
+# for gryffindor in sorted(gryffindor):
+#     print(gryffindor)
+
+def is_gryffindor(s):
+    return s["house"]=="Gryffindor"
+gryffindors = filter(is_gryffindor,students)
+
+for gryffindor in sorted(gryffindors,key=lambda s:s["name"]):
+    print(gryffindor["name"])
